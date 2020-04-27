dac3169a9d2303e73576679257cb2baa3e9be7be
Max <boborodada@gmail.com>
2020-04-27 21:25:43 +0300

    some changes

--- home_work5.py
+++ home_work5.py
@@ -10,34 +10,35 @@
 #   табуляцией, а строки — переносами строк (символ новой строки). При этом после
 #   каждой строки не должно быть символа табуляции и в конце не должно быть
 #   переноса строки.
+from typing import List, Union
 
 
 class Matrix:
     """Some class"""
 
-    def __init__(self, bag_of_bags):
+    def __init__(self, bag_of_bags: List[list]) -> None:
         self.bag_of_bags = bag_of_bags.copy()
 
-    def size(self):
+    def size(self) -> tuple:
         """Method witch returns number of strings as 'm'
            and number of columns as 'n' in self instance's matrix"""
         m = len(self.bag_of_bags)
         n = len(self.bag_of_bags[0])
         return m, n
 
-    def transpose(self):
+    def transpose(self) -> List[list]:
         """Method witch returns transposed self instance's matrix"""
-        t_arr = list(map(list, (zip(*self.bag_of_bags))))
-        return t_arr
+        self.bag_of_bags = list(map(list, (zip(*self.bag_of_bags))))
+        return self.bag_of_bags
 
     @classmethod
-    def create_transposed(cls, matr):
+    def create_transposed(cls, matr) -> 'Matrix':
         """Method witch creates class's instance
            by transposing the received matrix """
         t_matr = list(map(list, (zip(*matr))))
         return cls(t_matr)
 
-    def __add__(self, other):
+    def __add__(self, other: 'Matrix') -> 'Matrix':
         """Addition of self matrix with other matrix.
            If matrices sizes are different -> raise 'MatrixSizeError' """
         if self.size() == other.size():
@@ -48,12 +49,12 @@ class Matrix:
                           f' matrix1 {self.size()} and matrix2 {other.size()}')
         return Matrix(rez)
 
-    def __mul__(self, other):
+    def __mul__(self, other: Union[int, float]) -> 'Matrix':
         """Multiplies the matrix by a scalar number"""
         rez = [[el * abs(other) for el in bag] for bag in self.bag_of_bags]
         return Matrix(rez)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Turn matrix to string,
            where between columns is '\t', between strings is '\n'."""
         return '\n'.join(['\t'.join([str(el) for el in bag])
@@ -64,7 +65,7 @@ matrix1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 matrix2 = Matrix([[2, 3, 4], [5, 6, 7], [8, 9, 0]])
 
 print(f'matrix1 -> str\n'
-      f'{str(matrix1)}')
+      f'{str(matrix1)}\n')
 
 print(f'matrix1\n'
       f'{str(matrix1)}\n'
